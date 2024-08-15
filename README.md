ccd30b140ee26f258f38eba9f61f0d3417dda24d
Bashkim Kasa <bashkim.kasa@goto.com>
2024-08-15 16:43:52 -0400

    feat: added initial code and readme (sample)

---
+++ README.md
@@ -0,0 +1,82 @@
+# slack-bot-azureopenai-sample
+This is a sample slack app/bot that interacts with Azure OpenAI and Azure AI Search for RAG (Retrieval-Augmented Generation).
+
+
+## Description
+This is a sample python app slack app/bot that listens for incoming messages from channels the app is installed/invited in, opens a thread if not already in a thread and answer's the question if the grounding data is sufficient. It ignores all other events like message deletes for updates.
+In providing an answer it makes a call to Azure OpenAI with Azure Search datasources for RAG data. It provides the search endpoint, index name and semantic configuration to use for grounding the model.
+If there are additional requests from the user in the thread, it downloads all previous requests (from the user) and responses (from the bot) in the thread and adds them to the query in order to provide the model with better grounding data.
+When a post from a new user in the the thread is entered that thread is considered final and the bot no longer responds to it anymore.
+This is a simple app to demonstrate the functionality. It has minimal exception handling and support cases.
+
+
+## App Settings And Configuration 
+Please create a [`slack app`](https://api.slack/com/apps) and do the following:
+1. Go to `OAuth & Permissions`
+  1. Go to `Scopes` section and add the `chat:write` scope to it
+  2. Get the Bot User OAuth Token store somewhere for later use.
+  3. At this point you can re-install the app to the workspace (but it will be done later anyway when additional scopes are added from the `Event Subscriptions`)
+
+2. For local Development, go to `Socket Mode` and enable it.
+   1. When enablig `Socket Mode`, you will get an app level token which you can give it some name to your liking.
+   2. Store the app level token somewhere for later use.
+   3. Leave the default app level scope as is (`connections:write`)
+
+3. Go to `Event Subscriptions` for subscribing to specific events
+  1. First turn on `Enable Events`
+  2. When using `Socket Mode` (for development) above you do not need to provide a Request URL, but for production you'll need to provide the Request URL where slack will be sending the events to.
+  3. In `Subscribe to bot events` subscribe to the following events:
+    1. `message.channels` -> A message was posted to a channel (publich) that the bot is invited in.
+    2. `message.groups`   -> A message was posted to a private channel that the bot is invited in.
+    3. You can add additinal optional events if needed (like message:im) for direct message with the bot.
+    3. Save changes.
+
+4. At this point you should see a warning on the top of your screen indicating to re-install the app in your workspace in order for the changes to take affect. Please go ahead and re-install the app and in the subsequent screen Allow (authorize) the new scopes.
+
+5. Go to `App Home`
+  1. Turn on the feature `Always Show My Bot as Online`
+  2. If you want to change the name you can do so in the section `Your App's Presense in Slack`
+  3. In the `Show Tabs` section check the box that says `Allow users to send Slash commands and messages from the messages tab`. (this setting may take a couple of minutes to propagate to slack)
+
+
+## Getting started
+Setup a virtual environment (optional)
+```sh
+python -m venv .venv
+```
+
+Active the virtual environment (optional)
+```sh
+# For Linux:
+source .venv/bin/activate
+
+# For windows (gitbash etc.):
+source .venv/Scripts/activate
+```
+
+Install libraries
+```sh
+pip install slack_bolt
+pip install openai
+```
+
+Setup environment variables
+```sh
+# Slack Bot Variables
+export SLACK_BOT_TOKEN=<fill-in-value, example: xoxb-ABC...>
+export SLACK_APP_TOKEN=<fill-in-value, example: xapp-1-ABC...>
+
+# Azure AI Variables
+export AZURE_AI_ENDPOINT=<fill-in-value, example: https://my-bk-openai.openai.azure.com/>
+export AZURE_AI_DEPLOYMENT=<fill-in-value, example: gpt-35-turbo-16k>
+export AZURE_AI_API_KEY=<fill-in-value>
+export AZURE_AI_SEARCH_ENDPOINT=<fill-in-value, example: https://my-bk-aisearch.search.windows.net>
+export AZURE_AI_SEARCH_KEY=<fill-in-value>
+export AZURE_AI_SEARCH_INDEX=<fill-in-value, example: vector-my-ref-data-1723475267472>
+export AZURE_AI_SEARCH_SEMANTIC_CONFIGURATION=<fill-in-value, example: vector-my-ref-data-1723475267472-semantic-configuration>
+```
+
+Run python app.py file
+```sh
+python app.py
+```
---
+++ app.py
@@ -0,0 +1,180 @@
+import os
+import re
+import json
+from slack_bolt import App
+from slack_bolt.adapter.socket_mode import SocketModeHandler
+from openai import AzureOpenAI
+import logging
+
+# Logging configuration
+logging.basicConfig(level=logging.INFO)
+
+# Get environment variables (with defaults) TODO: Remove defaults in production
+# Slack environment variables
+slack_bot_token = os.getenv("SLACK_BOT_TOKEN")
+slack_app_token = os.getenv("SLACK_APP_TOKEN")
+
+# Azure OpenAI environment variables
+azure_ai_endpoint = os.getenv("AZURE_AI_ENDPOINT")
+azure_ai_deployment = os.getenv("AZURE_AI_DEPLOYMENT")
+azure_ai_api_key = os.getenv("AZURE_AI_API_KEY")
+azure_ai_search_endpoint = os.getenv("AZURE_AI_SEARCH_ENDPOINT")
+azure_ai_search_key = os.getenv("AZURE_AI_SEARCH_KEY")
+azure_ai_search_index = os.getenv("AZURE_AI_SEARCH_INDEX")
+azure_ai_search_semantic_configuration = os.getenv("AZURE_AI_SEARCH_SEMANTIC_CONFIGURATION")
+
+# Initialize Azure OpenAI client
+client = AzureOpenAI(
+    azure_endpoint=azure_ai_endpoint,
+    api_key=azure_ai_api_key,
+    api_version="2024-05-01-preview",
+)
+
+# Create function to call Azure OpenAI
+def call_azure_openai(messages):
+    completion = client.chat.completions.create(
+        model=azure_ai_deployment,
+        messages=messages,
+        max_tokens=800,
+        temperature=0,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0,
+        stop=None,
+        stream=False,
+        extra_body={
+            "data_sources": [{
+                "type": "azure_search",
+                "parameters": {
+                    "endpoint": f"{azure_ai_search_endpoint}",
+                    "index_name": f"{azure_ai_search_index}",
+                    "semantic_configuration": f"{azure_ai_search_semantic_configuration}",
+                    "query_type": "semantic",
+                    "fields_mapping": {
+                        "content_fields_separator": "\n",
+                        "content_fields": None,
+                        "filepath_field": None,
+                        "title_field": "title",
+                        "url_field": "url",
+                        "vector_fields": [
+                            "text_vector"
+                        ]
+                    },
+                    "in_scope": True,
+                    "role_information": "You are an AI assistant that helps people find information.",
+                    "filter": None,
+                    "strictness": 3,
+                    "top_n_documents": 3,
+                    "authentication": {
+                        "type": "api_key",
+                        "key": f"{azure_ai_search_key}"
+                    }
+                }
+            }]
+        }
+    )
+    return completion.to_dict()
+
+# Parse the response from Azure OpenAI and return content and citation url of the top 3 search results (dedubed)
+def parse_azure_openai_response(azure_openai_response):
+    unavailable_response = "The requested information is not available in the retrieved data. Please try another query or topic."
+    message = azure_openai_response.get("choices")[0].get("message")
+    message_content = message.get("content")
+    message_citations = message.get("context").get("citations")
+    if message_content is None or unavailable_response in message_content:
+        return unavailable_response
+    
+    # Cleanup the message, extract the citation urls from the response, dedub them and construct the response message
+    
+    # # Clean up the message - remove from the message the [doc1][docn]] pattern at the end of the message
+    # message_content = message_content.rsplit("\n\n[doc", 1)[0] or message_content
+
+    # # Remove [doc1] through [docn] instances from anywhere in the message
+    # message_content = re.sub(r" \[doc\d+\]", "", message_content)
+
+    citation_urls = []
+    for citation in message_citations:
+        citation_urls.append(citation.get("url"))
+
+    # Dedub the citation urls
+    citation_urls = list(set(citation_urls))
+    
+    # Construct the response message with citation urls as a list of hyperlinks if available
+    response_message = f"{message_content}\n\n"
+    if len(citation_urls) > 0:
+        response_message += "*Sources*:\n"
+        citation_number = 0
+        for url in citation_urls:
+            citation_number += 1
+            response_message += f"{citation_number}. <{url}>\n"
+
+    return response_message
+
+
+# Function to fetch all messages in a thread given the channel and thread_ts
+def get_thread_messages(channel, thread_ts):
+    # Get all messages in the thread
+    thread_response = app.client.conversations_replies(channel=channel, ts=thread_ts)
+    return thread_response["messages"]
+
+# Function to check if another user other than the original user and the bot posted in the thread 
+def external_user_added(messages):
+    original_user = messages[0]["user"]
+    for message in messages:
+        if message["user"] != original_user and message.get("bot_id", None) is None:
+            return True
+    return False
+
+# Function to add the history data to the messages list for conversation query
+def add_history_to_messages(incoming_message, messages, thread_messages):
+    for thread_message in thread_messages:
+        if thread_message.get("bot_id", None) is not None:
+            # Remove the sources from the bot's message that were added in the previous iterations
+            thread_message_text = re.sub(r"\n\n\*Sources\*:\n\d+\. <.+>\n", "", thread_message["text"])
+            messages.append({"role": "assistant", "content": thread_message_text})
+        # If the message from the thread is same as incoming message (or another user), skip it
+        elif incoming_message["ts"] != thread_message["ts"] and incoming_message["user"] == thread_messages[0]["user"]:
+            messages.append({"role": "user", "content": thread_message["text"]}) 
+    
+# Initializes your app with your bot token and signing secret
+app = App(
+    token=slack_bot_token
+)
+
+# Listens to incoming messages from channels the app is installed in
+@app.message("")
+def message_handler(message, say, logger):
+    # Initialize the messages list for the conversation query with the system message
+    messages = [{
+        "role": "system",
+        "content": "Answer only questions related to the topic of the conversation. Do not provide information or citations that not relevant to the conversation. Please include links from the citation content to the response message."
+    }]
+    logger.debug(json.dumps(message, indent=2))
+    thread_ts = message.get("thread_ts", None) or message["ts"]
+    ongoing_thread = "thread_ts" in message and "ts" in message
+    if ongoing_thread:
+        thread_messages = get_thread_messages(message["channel"], message["thread_ts"])
+        logger.debug(json.dumps(thread_messages, indent=2))
+        if external_user_added(thread_messages):
+            logger.debug(f"Another user added to the thread. Stopping the conversation with chatbot.")
+            return
+        else:
+            add_history_to_messages(message, messages, thread_messages)
+
+    # Add the user's message to the messages list
+    messages.append({"role": "user", "content": message["text"]})
+
+    azure_openai_response = call_azure_openai(messages)
+    logger.debug(json.dumps(azure_openai_response, indent=2))
+    parsed_response = parse_azure_openai_response(azure_openai_response)
+    say(f"Hi there, <@{message['user']}>\n\n{parsed_response}", thread_ts=thread_ts)
+
+# Ignore any other event
+@app.event("message")
+def event_handler(event, logger):
+    logger.debug(json.dumps(event, indent=2))
+
+# Start your app
+if __name__ == "__main__":
+    SocketModeHandler(app, slack_app_token).start()
+
